32b0d673a5050c7e7fdac106b74bbc31215bd9d0
phebbar12 <paree.hebbar@gmail.com>
2025-05-13 20:41:53 -0700

    saving runs

--- nbai/main.py
+++ nbai/main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 
 model = YOLO("models/ball_detector_model.pt")
 
-results = model.predict("input_videos/video_1.mp4", save=True)
+results = model.track("input_videos/video_1.mp4", save=True)
 print(results)
 print("==========================")
 for box in results[0].boxes:
